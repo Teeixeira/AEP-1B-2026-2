78c00c1301aad06dc5d8f5c4a59f9da729d08e38
ViniciusAlmeid4 <cmpalmeidaa@gmail.com>
2026-08-25 20:16:41 -0300

    refactor(env + host): removing swagger host so localhost and 127.0.0.1 area accepted and moving .env to backend

--- backend/main.go
+++ backend/main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/joho/godotenv"
 
 	swaggerFiles "github.com/swaggo/files"
-    ginSwagger "github.com/swaggo/gin-swagger"
+	ginSwagger "github.com/swaggo/gin-swagger"
 
 	_ "AEP-1B-2026-2/docs"
 )
@@ -23,7 +23,6 @@ import (
 // @title AEP API
 // @version 1.0
 // @description API for the AEP project.
-// @host localhost:8080
 // @BasePath /api
 func main() {
 
@@ -44,18 +43,18 @@ func main() {
 	databaseMongo := client.Database("AEP")
 
 	router := gin.Default()
-	
-    swaggerHandler := ginSwagger.WrapHandler(swaggerFiles.Handler)
 
-    router.GET("/swagger/*any", func(c *gin.Context) {
-        if c.Param("any") == "/" {
-            c.Redirect(http.StatusFound, "/swagger/index.html")
-            return
-        }
+	swaggerHandler := ginSwagger.WrapHandler(swaggerFiles.Handler)
 
-        swaggerHandler(c)
-    })
-	
+	router.GET("/swagger/*any", func(c *gin.Context) {
+		if c.Param("any") == "/" {
+			c.Redirect(http.StatusFound, "/swagger/index.html")
+			return
+		}
+
+		swaggerHandler(c)
+	})
+
 	api := router.Group("/api")
 
 	usuarioRepository := repositories.NewUsuarioRepository(databaseMongo)
@@ -88,104 +87,11 @@ func main() {
 	api.DELETE("/crimes/:id", crimeHandler.Delete)
 
 	log.Println("Servidor rodando na porta 8080")
+	log.Println("Swagger path: /swagger/index.html")
 
 	err = router.Run(":8080")
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// ctx, cancel := context.WithTimeout(
-	// 	context.Background(),
-	// 	10*time.Second,
-	// )
-	// defer cancel()
-
-	// usuario := models.Usuario{
-	// 	Nome:  "Leonardo",
-	// 	Email: "leonardo@email.com",
-	// 	Senha: "123456",
-	// 	Ativo: true,
-	// }
-
-	// err = repository.Create(ctx, &usuario)
-
-	// if err != nil {
-	// 	log.Fatal("Erro ao criar usuário:", err)
-	// }
-
-	// fmt.Println("Usuário criado com suscesso!")
-	// fmt.Println("ID:", usuario.ID)
-
-	// usuarios, err := repository.FindAll(ctx)
-
-	// if err != nil {
-	// 	log.Fatal("Erro ao buscar usuários:", err)
-	// }
-
-	// fmt.Println("Usuários encontrados:")
-
-	// for _, usuario := range usuarios {
-	// 	fmt.Println(usuario)
-	// }
-
-	// usuarioEncontrado, err := repository.FindByID(
-	// 	ctx,
-	// 	usuario.ID,
-	// )
-
-	// if err != nil {
-	// 	log.Fatal("Erro ao buscar usuário:", err)
-	// }
-
-	// fmt.Println("Usuário encontrado:")
-	// fmt.Println("ID:", usuarioEncontrado.ID)
-	// fmt.Println("Nome:", usuarioEncontrado.Nome)
-	// fmt.Println("Email:", usuarioEncontrado.Email)
-	// fmt.Println("Ativo:", usuarioEncontrado.Ativo)
-
-	// fmt.Println("=============== UPDATE TEST ===============")
-
-	// usuarioAtualizado := models.Usuario{
-	// 	Nome:  "Leonardo Atualizado",
-	// 	Email: "leonardo.novo@email.com",
-	// 	Senha: "654321",
-	// 	Ativo: true,
-	// }
-
-	// err = repository.Update(
-	// 	ctx,
-	// 	usuario.ID,
-	// 	usuarioAtualizado,
-	// )
-
-	// if err != nil {
-	// 	log.Fatal("Erro ao atualizar usuário:", err)
-	// }
-
-	// fmt.Println("Usuário atualizado com sucesso!")
-
-	// fmt.Println("=============== DELETE TEST ===============")
-
-	// err = repository.Delete(
-	// 	ctx,
-	// 	usuario.ID,
-	// )
-
-	// if err != nil {
-	// 	log.Fatal("Erro ao excluir usuário!", err)
-	// }
-
-	// fmt.Println("Usuário deletado com sucesso!")
-
-	// fmt.Println("=============== DELETE CONFIRMATION TEST ===============")
-
-	// _, err = repository.FindByID(
-	// 	ctx,
-	// 	usuario.ID,
-	// )
-
-	// if err != nil {
-	// 	fmt.Println("Usuário não encontrado após a exclusão!", err)
-	// }
 }
