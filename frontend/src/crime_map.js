adcf58b79eec4d46e55a0952a4afc63a713a9032
ViniciusAlmeid4 <cmpalmeidaa@gmail.com>
2026-08-26 21:45:46 -0300

    feat(map): adding leaflet template

---
+++ frontend/src/crime_map.js
@@ -0,0 +1,18 @@
+import L from 'leaflet';
+import markerIconUrl from "leaflet/dist/images/marker-icon.png";
+import markerIconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadowUrl from "leaflet/dist/images/marker-shadow.png";
+
+export function setupMap(element) {
+    L.Icon.Default.prototype.options.iconUrl = markerIconUrl;
+    L.Icon.Default.prototype.options.iconRetinaUrl = markerIconRetinaUrl;
+    L.Icon.Default.prototype.options.shadowUrl = markerShadowUrl;
+    L.Icon.Default.imagePath = ""; 
+
+    const map = L.map('map').setView([-23.4213, -51.9331], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+}
